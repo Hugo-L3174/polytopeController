ce8c3915dc200df45fba4e98f7b554d2be4d479b
Hugo L <lefevrehugo@ymail.com>
2024-10-04 15:06:59 +0900

    remove stabiliplus dependency

--- src/PolytopeController.cpp
+++ src/PolytopeController.cpp
@@ -73,130 +73,130 @@ void PolytopeController::reset(const mc_control::ControllerResetData & reset_dat
   mc_control::fsm::Controller::reset(reset_data);
 }
 
-void PolytopeController::updateContactSet(const std::vector<mc_rbdyn::Contact> & contacts, unsigned int robotIndex)
-{
-  const auto & robot = robots().robot(robotIndex);
-  mc_rtc::Configuration maxForces;
-  if(config_.has("surfacesMaxForces"))
-  {
-    maxForces = config_("surfacesMaxForces");
-  }
+// void PolytopeController::updateContactSet(const std::vector<mc_rbdyn::Contact> & contacts, unsigned int robotIndex)
+// {
+//   const auto & robot = robots().robot(robotIndex);
+//   mc_rtc::Configuration maxForces;
+//   if(config_.has("surfacesMaxForces"))
+//   {
+//     maxForces = config_("surfacesMaxForces");
+//   }
 
-  // XXX can we avoid allocating a new contact set every time?
-  contactSet_ = std::make_shared<ContactSet>(false);
+//   // XXX can we avoid allocating a new contact set every time?
+//   contactSet_ = std::make_shared<ContactSet>(false);
 
-  contactSet_->mass(robots().robot(robotIndex).mass());
-  contactSet_->setFrictionSides(6);
+//   contactSet_->mass(robots().robot(robotIndex).mass());
+//   contactSet_->setFrictionSides(6);
 
-  for(const auto & contact : contacts)
-  {
-    if(contact.r1Index() == robotIndex || contact.r2Index() == robotIndex)
-    {
-      const auto & surface_name =
-          (contact.r1Index() == robotIndex) ? contact.r1Surface()->name() : contact.r2Surface()->name();
-      const auto & surface = robot.surface(surface_name);
+//   for(const auto & contact : contacts)
+//   {
+//     if(contact.r1Index() == robotIndex || contact.r2Index() == robotIndex)
+//     {
+//       const auto & surface_name =
+//           (contact.r1Index() == robotIndex) ? contact.r1Surface()->name() : contact.r2Surface()->name();
+//       const auto & surface = robot.surface(surface_name);
 
-      const auto & bodyName = surface.bodyName();
-      const auto & body_PT = robot.bodyPosW(bodyName);
+//       const auto & bodyName = surface.bodyName();
+//       const auto & body_PT = robot.bodyPosW(bodyName);
 
-      const auto & points = surface.points();
-      int ptCpt = 0; // point counter
-      double mu = contact.friction(); // get the friction coef h
+//       const auto & points = surface.points();
+//       int ptCpt = 0; // point counter
+//       double mu = contact.friction(); // get the friction coef h
 
-      double fmax;
-      if(maxForces.has(surface_name))
-      {
-        fmax = maxForces(surface_name);
-      }
-      else
-      {
-        // XXX assuming max force is mass
-        fmax = robot.mass() * 9.81;
-      }
+//       double fmax;
+//       if(maxForces.has(surface_name))
+//       {
+//         fmax = maxForces(surface_name);
+//       }
+//       else
+//       {
+//         // XXX assuming max force is mass
+//         fmax = robot.mass() * 9.81;
+//       }
 
-      double fmin = 0; // TODO set the same for min forces?
-      ContactType type;
-      if(config_.has("constrainedSurfaces"))
-      {
-        auto constrainedSurfaces = config_("constrainedSurfaces");
-        if(constrainedSurfaces.has(surface_name))
-        {
-          type = ContactType::constrained;
-        }
-        else
-        {
-          type = ContactType::support;
-        }
-      }
-      else
-      {
-        type = ContactType::support;
-      }
+//       double fmin = 0; // TODO set the same for min forces?
+//       ContactType type;
+//       if(config_.has("constrainedSurfaces"))
+//       {
+//         auto constrainedSurfaces = config_("constrainedSurfaces");
+//         if(constrainedSurfaces.has(surface_name))
+//         {
+//           type = ContactType::constrained;
+//         }
+//         else
+//         {
+//           type = ContactType::support;
+//         }
+//       }
+//       else
+//       {
+//         type = ContactType::support;
+//       }
 
-      for(const auto & point : points)
-      {
-        auto pos = body_PT.rotation().transpose() * point.translation() + body_PT.translation();
+//       for(const auto & point : points)
+//       {
+//         auto pos = body_PT.rotation().transpose() * point.translation() + body_PT.translation();
 
-        Eigen::Matrix4d homTrans = Eigen::Matrix4d::Identity();
-        homTrans.block<3, 3>(0, 0) = body_PT.rotation().transpose() * point.rotation().transpose();
-        homTrans.block<3, 1>(0, 3) = pos;
+//         Eigen::Matrix4d homTrans = Eigen::Matrix4d::Identity();
+//         homTrans.block<3, 3>(0, 0) = body_PT.rotation().transpose() * point.rotation().transpose();
+//         homTrans.block<3, 1>(0, 3) = pos;
 
-        const auto ptName = surface.name() + "_" + std::to_string(ptCpt);
-        contactSet_->addContact(ptName, homTrans, mu, fmax, fmin, type);
-        ptCpt++;
-      }
-    }
-  }
+//         const auto ptName = surface.name() + "_" + std::to_string(ptCpt);
+//         contactSet_->addContact(ptName, homTrans, mu, fmax, fmin, type);
+//         ptCpt++;
+//       }
+//     }
+//   }
 
-  // Adding the accelerations
-  Eigen::Vector3d acceleration;
+//   // Adding the accelerations
+//   Eigen::Vector3d acceleration;
 
-  acceleration << 0.0, 0.0, -9.81;
-  contactSet_->addCoMAcc(acceleration);
+//   acceleration << 0.0, 0.0, -9.81;
+//   contactSet_->addCoMAcc(acceleration);
 
-  acceleration << 0.6, 0, -9.81;
-  contactSet_->addCoMAcc(acceleration);
+//   acceleration << 0.6, 0, -9.81;
+//   contactSet_->addCoMAcc(acceleration);
 
-  acceleration << 0, 0.6, -9.81;
-  contactSet_->addCoMAcc(acceleration);
+//   acceleration << 0, 0.6, -9.81;
+//   contactSet_->addCoMAcc(acceleration);
 
-  acceleration << -0.6, 0, -9.81;
-  contactSet_->addCoMAcc(acceleration);
+//   acceleration << -0.6, 0, -9.81;
+//   contactSet_->addCoMAcc(acceleration);
 
-  acceleration << 0, -0.6, -9.81;
-  contactSet_->addCoMAcc(acceleration);
-}
+//   acceleration << 0, -0.6, -9.81;
+//   contactSet_->addCoMAcc(acceleration);
+// }
 
-void PolytopeController::updateObjective(MCStabilityPolytope * polytope_,
-                                         Eigen::Vector3d currentPos,
-                                         Eigen::Vector3d & objective)
-{
-  double filterCoeff = 0.7;
-  auto planes = polytope_->constraintPlanes();
+// void PolytopeController::updateObjective(MCStabilityPolytope * polytope_,
+//                                          Eigen::Vector3d currentPos,
+//                                          Eigen::Vector3d & objective)
+// {
+//   double filterCoeff = 0.7;
+//   auto planes = polytope_->constraintPlanes();
 
-  // XXX version with objective towards middle of polytope
-  Eigen::Vector3d chebichev = polytope_->chebichevCenter();
-  Eigen::Vector3d bary = polytope_->baryCenter();
-  // lowpass filtering of center of polytope (very noisy from every computation)
-  // lowPassPolyCenter_.update(bary);
-  // bary = lowPassPolyCenter_.eval();
-  Eigen::Vector3d filteredObjective = (1 - filterCoeff) * currentPos + filterCoeff * bary;
-  // lowPassPolyCenter_.update(chebichev);
-  // chebichev = lowPassPolyCenter_.eval();
-  // Eigen::Vector3d filteredObjective = (1 - chebichevCoef_) * currentPos + chebichevCoef_ * chebichev;
-  // objective = polytope_.objectiveInPolytope(filteredObjective);
+//   // XXX version with objective towards middle of polytope
+//   Eigen::Vector3d chebichev = polytope_->chebichevCenter();
+//   Eigen::Vector3d bary = polytope_->baryCenter();
+//   // lowpass filtering of center of polytope (very noisy from every computation)
+//   // lowPassPolyCenter_.update(bary);
+//   // bary = lowPassPolyCenter_.eval();
+//   Eigen::Vector3d filteredObjective = (1 - filterCoeff) * currentPos + filterCoeff * bary;
+//   // lowPassPolyCenter_.update(chebichev);
+//   // chebichev = lowPassPolyCenter_.eval();
+//   // Eigen::Vector3d filteredObjective = (1 - chebichevCoef_) * currentPos + chebichevCoef_ * chebichev;
+//   // objective = polytope_.objectiveInPolytope(filteredObjective);
 
-  // XXX version with current pos as objective
-  objective = polytope_->objectiveInPolytope(currentPos);
-  if(polytope_->configToChange_)
-  {
-    // XXX needed only if using uncolored polyhedron implementation
-    // polytope_.removeFromGUI(*gui());
-    // polytope_.addToGUI(*gui());
-  }
-  // prevent polytope projection to lower objective (from polytope form, closest point might be lower)
-  if(objective.z() < currentPos.z())
-  {
-    objective.z() = currentPos.z();
-  }
-}
+//   // XXX version with current pos as objective
+//   objective = polytope_->objectiveInPolytope(currentPos);
+//   if(polytope_->configToChange_)
+//   {
+//     // XXX needed only if using uncolored polyhedron implementation
+//     // polytope_.removeFromGUI(*gui());
+//     // polytope_.addToGUI(*gui());
+//   }
+//   // prevent polytope projection to lower objective (from polytope form, closest point might be lower)
+//   if(objective.z() < currentPos.z())
+//   {
+//     objective.z() = currentPos.z();
+//   }
+// }
--- src/PolytopeController.h
+++ src/PolytopeController.h
@@ -4,8 +4,10 @@
 #include <mc_control/mc_controller.h>
 
 #include <mc_tasks/DCM_VRPTask.h>
-#include "utils/MCStabilityPolytope.h"
+// #include "utils/MCStabilityPolytope.h"
 #include <mc_dynamic_polytopes/DynamicPolytope.h>
+
+#include <eigen-quadprog/QuadProg.h>
 
 #include "api.h"
 
@@ -20,15 +22,15 @@ struct PolytopeController_DLLAPI PolytopeController : public mc_control::fsm::Co
   // legacy function to update the contact set for stabiliplus balance computation
   void updateContactSet(const std::vector<mc_rbdyn::Contact> & contacts, unsigned int robotIndex);
   // legacy function to update mcstabilitypolytope (wrapper for stabiliplus)
-  void updateObjective(MCStabilityPolytope * polytope_, Eigen::Vector3d currentPos, Eigen::Vector3d & objective);
+  // void updateObjective(MCStabilityPolytope * polytope_, Eigen::Vector3d currentPos, Eigen::Vector3d & objective);
 
 private:
   sva::PTransformd wallPose_;
   bool firstPolyOK_ = false;
 
   // stabiliplus elements
-  std::shared_ptr<MCStabilityPolytope> robotPolytope_;
-  std::shared_ptr<ContactSet> contactSet_;
+  // std::shared_ptr<MCStabilityPolytope> robotPolytope_;
+  // std::shared_ptr<ContactSet> contactSet_;
 
   // mc_dynamic_polytopes element
   std::shared_ptr<DynamicPolytope> DCMPoly_;
@@ -38,4 +40,6 @@ private:
   // planes normals and offsets for eCMP region testing and constraint
   Eigen::MatrixX3d eCMPPlanesNormals_;
   Eigen::VectorXd eCMPPlanesOffsets_;
+
+  Eigen::QuadProgDense testSolver_;
 };
